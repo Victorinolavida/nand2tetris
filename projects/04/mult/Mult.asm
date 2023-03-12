da2352e35dc195435d2296b5db81c9cc67a85c4b
victorinoLavida <victorinolavida@ciencias.unam.mx>
2023-03-12 11:12:07 -0600

    project 5 started

---
+++ projects/04/mult/Mult.asm
@@ -0,0 +1,56 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//
+// This program only needs to handle arguments that satisfy
+// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+
+// Put your code here.
+
+@R0
+D=M
+
+@value
+M=D //value=R0
+
+@i
+M=1
+
+@result
+M=0
+
+(LOOP)
+    @i
+    D=M
+
+    @value
+    D=M-D
+
+    @STOP
+    D;JLT
+
+    @R1
+    D=M
+
+    @result
+    M = M + D
+
+    @i
+    M = M +1
+
+    @LOOP
+    0;JMP
+
+(STOP)
+    @result
+    D=M
+    @R2
+    M = D
+(END)
+    @END
+    0;JMP
+
