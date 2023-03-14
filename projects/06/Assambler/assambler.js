8126aaeaf2f5bbe30a190951f83073f151e68f17
victorinoLavida <victorinolavida@ciencias.unam.mx>
2023-03-14 09:45:45 -0600

    project 6 finished

---
+++ projects/06/Assambler/assambler.js
@@ -0,0 +1,46 @@
+const FOLDER_FILES = "/Users/victorino.ruiz/Desktop/nand2tetris/projects/06/";
+const fs = require("fs");
+const { translate } = require("./code");
+
+const args = process.argv.slice(2);
+function main() {
+  let FILE_PATH = "";
+
+  if (args.length === 0) throw new Error("nombre del archivo vacio");
+
+  const folders = [];
+  fs.readdirSync(FOLDER_FILES).forEach((file) => {
+    if (file.toString() !== "Assambler") {
+      folders.push(FOLDER_FILES + file + "/");
+    }
+  });
+
+  if (folders.length === 0) throw new Error("la carpeta 06 esta vacia");
+
+  while (folders.length !== 0) {
+    const folder = folders.shift();
+    fs.readdirSync(folder).forEach((file) => {
+      if (args[0] == file) {
+        FILE_PATH = folder + `${args[0]}`;
+      }
+    });
+  }
+  console.log(FILE_PATH);
+  if (!FILE_PATH) throw new Error(`No se encontro el archivo ${args[0]}`);
+
+  const data = fs.readFileSync(FILE_PATH, "utf8").split("\n").filter(Boolean);
+
+  const code_machine = translate(data);
+
+  //writing file
+  
+  fs.writeFileSync(`${args[0].replace('asm','hack')}`,code_machine.join("\n"));
+  console.log(`file ${args[0].replace('asm','hack')} saved`)
+}
+
+try {
+  main();
+  
+} catch (e) {
+  console.error("Error: ", e.message);
+}
---
+++ projects/06/Assambler/symbols.js
@@ -0,0 +1,70 @@
+
+
+const VARIABLES = {}
+let VARIABLES_INDICE = 16;
+
+const {PRE_DEF_SYMBOLS} = require('./constants')
+
+function symbols(instruction){
+   return PRE_DEF_SYMBOLS[instruction] || VARIABLES[instruction] || instruction;
+}
+
+function  is_label(instruction) {
+    return instruction.indexOf("\(") !== -1
+}
+
+function label_value(instruction,index) {
+
+    const left = instruction.indexOf("\(");
+    const rigth = instruction.indexOf("\)");
+
+    const label = instruction.slice(left+1,rigth);
+
+    if(!VARIABLES[label]){
+        VARIABLES[label] = ""+index
+    }
+}
+
+function is_variable(instruction){
+    // console.log(VARIABLES)
+    if(!instruction.startsWith("@")) return;
+    let label = instruction.slice(1);
+    if(PRE_DEF_SYMBOLS[label] &&instruction.indexOf("\(")===-1) return;
+    if(VARIABLES[label])return;
+    if(isNaN(label)){
+
+    VARIABLES[label] = VARIABLES_INDICE;
+    VARIABLES_INDICE++;
+    }
+}
+
+function remove_labels(code){
+    const code_without_labels = []
+    let counter = 0;
+
+    for(let i=0;i<code.length;i++){
+        let instruction = code[i];
+        if(is_label(instruction)){
+            label_value(instruction,i-counter);
+            counter ++;
+        }else{
+            code_without_labels.push(instruction)
+        }
+    }
+    
+    for(let i=0;i<code_without_labels.length;i++){
+        let instruction = code_without_labels[i];
+        // console.log(instruction,i)
+        if(instruction.startsWith("@")){
+            is_variable(instruction)
+            let label = instruction.slice(1)
+            code_without_labels[i] = "@"+symbols(label)
+        }
+    }
+    return code_without_labels
+}
+
+
+module.exports={
+    remove_labels
+}
